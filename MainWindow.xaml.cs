15b61b6688154b64a0dd974f326a7ec3999c26df
Robin Blom <robin.blom@hotmail.se>
2021-05-05 15:41:38 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PickACardUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string[] pickedCards = CardPicker.PickSomeCards((int)numberOfCards.Value);          // This method takes the array that the PickSomeCards-method returns. 
+            listOfCards.Items.Clear();                                                          // It clears the ListBox of any existing cards.
+
+            foreach(string card in pickedCards)                                                 // And then adds every card in the pickedCards-array to the ListBox.
+            {
+                listOfCards.Items.Add(card);
+            }
+        }
+    }
+}
